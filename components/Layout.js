21523d4cea7a86882d0fc0d68bcd12ee1167efdd
Jacob Capps <jcapps1@Jacobs-MacBook-Air.local>
2020-10-14 14:53:06 -0500

    Added client-side check

--- components/Layout.js
+++ components/Layout.js
@@ -38,7 +38,7 @@ const Layout = ({ children }) => {
       </li>
 
       {// if user is not authenticated, show the following links
-      !isAuth() && (
+      process.browser && !isAuth() && (
         <React.Fragment>
           <li className="nav-item">
             <Link href="/login">
@@ -53,7 +53,7 @@ const Layout = ({ children }) => {
         </React.Fragment>
       )}
 
-      {isAuth() && isAuth().role === "admin" && (
+      {process.browser && isAuth() && isAuth().role === "admin" && (
         <li className="nav-item ml-auto">
           <Link href="/admin">
             <a className="nav-link text-light">{isAuth().name}</a>
@@ -61,7 +61,7 @@ const Layout = ({ children }) => {
         </li>
       )}
 
-      {isAuth() && isAuth().role === "subscriber" && (
+      {process.browser && isAuth() && isAuth().role === "subscriber" && (
         <li className="nav-item ml-auto">
           <Link href="/user">
             <a className="nav-link text-light">{isAuth().name}</a>
@@ -69,7 +69,7 @@ const Layout = ({ children }) => {
         </li>
       )}
 
-      {isAuth() && (
+      {process.browser && isAuth() && (
         <li className="nav-item">
           <a onClick={logout} className="nav-link text-light">
             Logout
